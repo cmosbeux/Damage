2685be2529654133c056deb5bf84be5c2bee6f16
Cyrille <91067824+cmosbeux@users.noreply.github.com>
2022-11-24 22:42:22 +0100

    update animations

--- AdvReac_Test/animations/Make_gif.sh
+++ AdvReac_Test/animations/Make_gif.sh
@@ -2,3 +2,4 @@
 convert -delay 10 -loop 0 NoReinit_Nodal_Serial*.png NoReinit_Nodal_Serial.gif
 convert -delay 10 -loop 0 NoReinit_Nodal_Parallel*.png NoReinit_Nodal_Parallel.gif
 convert -delay 10 -loop 0 NoReinit_Elemental_Serial*.png NoReinit_Elemental_Serial.gif
+convert -delay 10 -loop 0 NoReinit_Elemental_Parallel*.png NoReinit_Elemental_Parallel.gif
